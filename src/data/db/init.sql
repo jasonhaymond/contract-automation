7583935b81bbd662095516e1caf8acfeb07f6d87
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2021-03-25 17:35:18 -0700

    Refine DB schema

--- src/data/db/init.sql
+++ src/data/db/init.sql
@@ -1,27 +1,28 @@
 CREATE TABLE IF NOT EXISTS execution (
-    execution_id INTEGER NOT NULL,
-    execution_timestamp CHAR(24) NOT NULL,
-    PRIMARY KEY (execution_id)
+    id INTEGER NOT NULL,
+    timestamp INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS drmm_device (
-    drmm_device_id INTEGER NOT NULL,
-    drmm_device_uid CHAR(36) NOT NULL UNIQUE,
-    drmm_device_site_uid CHAR(36) NOT NULL,
-    drmm_device_type SMALLINT NOT NULL,
-    drmm_device_hostname NVARCHAR(255),
-    drmm_device_description NVARCHAR(255),
-    drmm_device_ipv4_int CHAR(15),
-    drmm_device_ipv4_ext CHAR(15),
-    PRIMARY KEY (drmm_device_id),
+    id INTEGER NOT NULL,
+    uid BINARY(16) NOT NULL UNIQUE,
+    site_uid BINARY(16) NOT NULL,
+    type TINYINT NOT NULL,
+    hostname NVARCHAR(255),
+    description NVARCHAR(255),
+    ipv4_int BINARY(4),
+    ipv4_ext BINARY(4),
+    is_active TINYINT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE INDEX IF NOT EXISTS ix_drmm_device_site_uid
-ON drmm_device (drmm_device_site_uid);
+ON drmm_device (site_uid);
 
-CREATE TABLE IF NOT EXISTS drmm_device_mutation (
-    drmm_device_mutation_id INTEGER NOT NULL,
-    drmm_device_mutation_type SMALLINT NOT NULL,
-    PRIMARY KEY (drmm_device_mutation_id),
-    FOREIGN KEY (drmm_device_id) REFERENCES drmm_device (drmm_device_id)
+CREATE TABLE IF NOT EXISTS drmm_device_history (
+    id INTEGER NOT NULL,
+    type SMALLINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES drmm_device (id)
 );
