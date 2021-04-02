4dda5a877de7fbe8a061659e6f902218caca807f
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2021-04-02 11:39:18 -0700

    Sync work

--- src/data/db/index.js
+++ src/data/db/index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const Database = require('better-sqlite3');
 
 const { DB_PATH } = process.env;
+const dbInitScriptPath = path.join(__dirname, 'init.sql');
 
 const getDb = function() {
     const db = new Database(DB_PATH);
     db.pragma('PRAGMA foreign_keys = ON;');
 
-    const initSql = fs.readFileSync(path.join(__dirname, 'init.sql'), 'utf-8');
+    const initSql = fs.readFileSync(dbInitScriptPath, 'utf-8');
     db.exec(initSql);
 
     return db;
